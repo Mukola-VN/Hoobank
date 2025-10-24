f828403ea66a1a7015a148aea31d6f7f4af2d970
Mukola <vmv.vynnychuk98@gmail.com>
2025-10-24 16:20:15 +0300

    root

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// ВАЖЛИВО: заміни 'hoobank' на точну назву твого репозиторію,
+// якщо вона відрізняється
 export default defineConfig({
+  base: '/hoobank/',
   plugins: [react()],
 })
