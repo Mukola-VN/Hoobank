1c407ca0004d23a56656f52b789853b7eb13b4f7
Mukola <vmv.vynnychuk98@gmail.com>
2025-10-24 10:03:07 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
-import React from 'react';
+import styles from './style';
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testemonials,
+  Clients,
+  CTA,
+  Footer,
+} from './components';
 
 const App = () => {
   return (
-    <div className='bg-primary w-full overflow-hidden'>
-      <h1>Hello, World</h1>
+    <div className="bg-primary w-full overflow-hidden">
+
+      {/* Navbar */}
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      {/* Hero Section */}
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      {/* Main Content */}
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testemonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+
     </div>
   );
 };
